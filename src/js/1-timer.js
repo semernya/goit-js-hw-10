7b15bf3064c319dc11dbb36f0c9d79e1c8b63520
iknowueterent <semernya38@gmail.com>
2024-01-19 23:41:03 +0200

    add styles to snackbar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { default as iziToast, IziToastSettings } from 'izitoast';
-// import 'izitoast@1/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
